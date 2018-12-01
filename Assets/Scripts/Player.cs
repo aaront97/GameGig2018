0453f669f3e43313bb664ea006944ba6c3b8afb6
Kocolipy <33294859+Kocolipy@users.noreply.github.com>
2018-12-01 10:01:38 +0000

    Initial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(0f, 60f)]
+    public float _speed = 60f;
+
+    public Vector3 _jumpInitialVelocity = Vector3.up * 10f;
+
+    enum JumpState
+    {
+        NotJumping,
+        Jumping,
+    }
+    JumpState _jumpState = JumpState.NotJumping;
+
+    Vector3 _velocity = Vector3.zero;
+
+    void Update ()
+    {
+        if(!GameLogic.Instance.PlayerAlive)
+        {
+            return;
+        }
+
+        transform.position += _speed * Vector3.forward * Time.deltaTime;
+
+        switch(_jumpState)
+        {
+            case JumpState.NotJumping:
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    // switch to jumping
+                    Debug.Log("Started jumping");
+
+                    _velocity = _jumpInitialVelocity;
+
+                    _jumpState = JumpState.Jumping;
+                }
+
+                break;
+            case JumpState.Jumping:
+                transform.position += _velocity * Time.deltaTime;
+                _velocity += Physics.gravity * Time.deltaTime;
+                
+                if(transform.position.y <= 1f)
+                {
+                    var pos = transform.position;
+                    pos.y = 1f;
+                    transform.position = pos;
+
+                    _jumpState = JumpState.NotJumping;
+                }
+                break;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform _cameraTarget;
+
+    public float _lerpAmount = 0.1f;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+        transform.position = Vector3.Lerp(transform.position, _cameraTarget.position, _lerpAmount);
+	}
+}
